ef6e7815880ff7a61843fcd3f5ad39f35b87720c
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-02-28 21:18:32 +0200

    Colors in cells now randomed and can be uploaded to Google Drive.

--- 02_decorators/decorators.py
+++ 02_decorators/decorators.py
@@ -1,18 +1,28 @@
 import xlsxwriter
 import gdown
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 import random
 import timeit
 from random import randrange
 
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+gdrive = GoogleDrive()
+
+
 # ссылка на мой файл на гугл драйве
-url = 'https://drive.google.com/uc?id=1ThKn3qS75KCo6UeBVxxv5JFyCay0aKd_'
+# url = 'https://drive.google.com/uc?id=1ThKn3qS75KCo6UeBVxxv5JFyCay0aKd_'
 # # То, с каким именем будет выведен мой файл
-output = 'Testing.xlsx'
-gdown.download(url, output, quiet=False)
+# output = 'Testing.xlsx'
+# gdown.download(url, output, quiet=False)
 workbook = xlsxwriter.Workbook('Testing.xlsx')
-# Просто формат, красящий клетку в жёлтый
-rand_yellow = workbook.add_format()
-rand_yellow.set_bg_color('yellow')
+# Просто формат, красящий клетку в рандомний цвет
+color = ("#"+''.join(random.choice('0123456789ABCDEF') for j in range(6))
+             for i in range(1))
+color_str = ''.join(color)
+rand_cell_col = workbook.add_format()
+rand_cell_col.set_bg_color(color_str)
 
 
 # Функция для запуска таймера, в ней содержится декоратор
@@ -35,5 +45,9 @@ def random_numbers():
         for y in numbers:
             worksheet.write_column(x, y, [random.randint(1, 100)])
     for i in numbers:
-        worksheet.write_column(i, randrange(10), [random.randint(1, 100)], rand_yellow)
+        worksheet.write_column(i, randrange(10), [random.randint(1, 100)], rand_cell_col)
     workbook.close()
+
+file1 = gdrive.CreateFile({'title':'Testing.xlsx'})
+file1.SetContentFile('Testing.xlsx')
+file1.Upload()
