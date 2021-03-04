caea6a1598bfa5eaf245d4ce75425eeb907cb5d6
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-03-04 15:43:36 +0200

    Added printing of titles from Google Search

--- 04_bs4/bs4_testing.py
+++ 04_bs4/bs4_testing.py
@@ -5,6 +5,9 @@ results = 10
 page = requests.get(f"https://www.google.com/search?q={search}&num={results}")
 soup = BeautifulSoup(page.content, "html5lib")
 links = soup.findAll("a")
+heading_object=soup.find_all( 'h3' )
+for info in heading_object:
+    print(info.getText())
 for link in links :
     link_href = link.get('href')
     if "url?q=" in link_href and not "webcache" in link_href:
