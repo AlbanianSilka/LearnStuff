6075b54dddbe139d4b54b4d527d0d14fbdb352a9
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-03-04 15:50:01 +0200

    Little fixes

--- 04_bs4/bs4_testing.py
+++ 04_bs4/bs4_testing.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
 search = input("Search:")
-results = 10
-page = requests.get(f"https://www.google.com/search?q={search}&num={results}")
+page = requests.get(f"https://www.google.com/search?q=" + search)
 soup = BeautifulSoup(page.content, "html5lib")
 links = soup.findAll("a")
-heading_object=soup.find_all( 'h3' )
+heading_object = soup.find_all('h3')
 for info in heading_object:
     print(info.getText())
-for link in links :
+for link in links:
     link_href = link.get('href')
-    if "url?q=" in link_href and not "webcache" in link_href:
-        print (link.get('href').split("?q=")[1].split("&sa=U")[0])
+    if "url?q=" in link_href:
+        print(link.get('href').split("?q=")[1].split("&sa=U")[0])
