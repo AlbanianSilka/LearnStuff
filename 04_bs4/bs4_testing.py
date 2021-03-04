72b095aeb1ad7970f7f435ad10860ee2091463fb
AlbanianSilka <andrii.khavriuta@gmail.com>
2021-03-04 19:49:50 +0200

    I've rewrite code almost fully but now it prints title and its url more beautiful.

--- 04_bs4/bs4_testing.py
+++ 04_bs4/bs4_testing.py
@@ -1,14 +1,28 @@
 import requests
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 
+ua = UserAgent()
 search = input("Search:")
-page = requests.get(f"https://www.google.com/search?q=" + search)
-soup = BeautifulSoup(page.content, "html5lib")
-links = soup.findAll("a")
-heading_object = soup.find_all('h3')
-for info in heading_object:
-    print(info.getText())
-for link in links:
-    link_href = link.get('href')
-    if "url?q=" in link_href:
-        print(link.get('href').split("?q=")[1].split("&sa=U")[0])
+google_url = f"https://www.google.com/search?q=" + search
+response = requests.get(google_url, {"User-Agent": ua.random})
+soup = BeautifulSoup(response.text, "html.parser")
+
+result_div = soup.find_all('div', attrs={'class': 'ZINbbc'})
+
+links = []
+titles = []
+for r in result_div:
+    try:
+        link = r.find('a', href=True)
+        title = r.find('div', attrs={'class': 'vvjwJb'}).get_text()
+
+        if title != '':
+            titles.append(title)
+        link_href = link.get('href')
+        if "url?q=" in link_href:
+            printed_link = (link.get('href').split("?q=")[1].split("&sa=U")[0])
+            print(title)
+            print(printed_link)
+    except:
+        continue
